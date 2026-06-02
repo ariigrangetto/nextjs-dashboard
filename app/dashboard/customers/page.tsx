f0c2cdcdff89cae39df22b24eee90712bf3f662a
Arianna <ariigrangetto5@gmail.com>
2026-06-02 17:45:22 -0300

    feat: chapter 15

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,8 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Customers'
+};
 export default function CustomerPage() {
     return (
         <p>Customers Page</p>
