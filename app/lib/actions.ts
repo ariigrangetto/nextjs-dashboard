da32daa12f905b01131a24ca6481a32a385d0a31
Arianna <ariigrangetto5@gmail.com>
2026-06-02 10:16:38 -0300

    feat: chapter 13

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -9,24 +9,46 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
+    customerId: z.string({
+        invalid_type_error: "Please select a customer.",
+    }),
+    amount: z.coerce.number().gt(0, { message: "Please enter an amount greater than $0." }),
     //coerce funciona para convertir automaticamente un tipo de dato de entrada al tipo de dato que se espera de salida
-    status: z.enum(["pending", "paid"]),
+    status: z.enum(["pending", "paid"], {
+        invalid_type_error: "Please select an invoice status.",
+    }),
     date: z.string(),
 });
+
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+};
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 //omit crea un nuevo esquema a partir de un esquema ya existente, eliminando elementos especificos como en este caso el id y el date
 
-export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(prevState: State, formData: FormData) {
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get("customerId"),
         amount: formData.get("amount"),
         status: formData.get("status"),
     });
+
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: "Missing Fields. Failed to Create Invoice."
+        };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
 
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split("T")[0];
@@ -50,13 +72,21 @@ export async function createInvoice(formData: FormData) {
 //parse es la funcion principal que se utiliza para validar y asegurar que coincidan con un esquema especificado
 //safeParse en cambio, devuelve un objeto con una propiedad success
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+    const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get("customerId"),
         amount: formData.get("amount"),
         status: formData.get("status"),
     });
 
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: "Missing Fields. Failed to Update Invoice."
+        };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
 
     try {
